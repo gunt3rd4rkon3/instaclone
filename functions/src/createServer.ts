46d1f503771225fd4bfb901fe7a745d0151f156d
unknown <pruebaservers@gmail.com>
2020-09-24 21:11:58 +0200

    error video 87

--- functions/src/createServer.ts
+++ functions/src/createServer.ts
@@ -1,14 +1,33 @@
 import  * as  express from 'express'
+import * as admin from  'firebase-admin'
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+})
+
+const db = admin.firestore()
+const auth = admin.auth()
 export default () => {
- const app = express()
- app.use((req:any, res, next) => {
-     console.log('soy un middleware')
-     req.user = 'lala'
-     next()
- })
- app.get('/posts', (req:any, res) => {
-     console.log(req.user)
-     res.send('Holaaaaa wey')
- })
- return app
+    const app = express()
+    app.use(async (req, res, next) => {
+        const token:any = req.headers.authorization
+        try {
+        const {uid, email} = await auth.verifyIdToken(token)
+        const snap = await db.collection('users').doc(uid).get()
+        const user = snap.data()
+        Object.assign(req, {
+            ...user,
+            uid,
+            email,
+        })
+        next()
+    } catch(e) {
+        res.status(400).send('Error al actualizar')
+    }
+    })
+
+    app.get('/posts', (req, res) => {
+        res.send('Holaaaaa wey')
+    })
+    return app
 }
